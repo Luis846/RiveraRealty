d95c36741424eda85005c397932a898518b45ff2
Luis <luis.rivera846@gmail.com>
2019-10-15 13:45:46 -0400

    removed email from settings

--- rep/settings.py
+++ rep/settings.py
@@ -144,12 +144,6 @@ MESSAGE_TAGS = {
 }
 
 
-#email
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER ='blvckowlshop@gmail.com'
-EMAIL_HOST_PASSWORD ='roomba23!'
-EMAIL_USE_TLS=True
 
 #production server
 try:
